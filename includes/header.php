39da4a1521b0ad4930ca69379c81551e7c5e4c71
Brenden Evans <bhevans17@gmail.com>
2020-09-06 15:38:43 -0700

    HF

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
     <link href="/doctr/css/style.css" type="text/css" rel="stylesheet">
     <meta name="author" content="Brenden Evans">
     <meta name="description" content="Website template for a medical type business.">
+    <link rel="icon" href="/doctr/./img/sitting-3.svg">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Doctr | For Your Health</title>
 </head>
